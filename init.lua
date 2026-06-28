1d569182933ff3366648aa9cce7af2ab633f122b
Toon Van Deuren <toonvandeuren@hotmail.com>
2026-06-28 23:31:03 +0200

    feat: initial nvim setup

---
+++ init.lua
@@ -0,0 +1,4 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = "#74c1ff", bg = nil })
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  -- Configure LazyVim to load your preferred colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_default", -- Replace with your desired colorscheme
+    },
+  },
+}
